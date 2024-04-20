7260a227e21cdf9fef0f1ee7ffc471615703e5b8
gc292 <guicaria52@gmail.com>
2024-04-20 17:25:19 +0100

    update

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,8 +1,17 @@
 package breakout;
 
+import javax.swing.SwingUtilities;
+
+import utils.GameController;
+
 public class Main {
 	public static void main(String[] args) {
-		
+GameController gameController = null;
+        
+        // Crie um novo objeto Breakout e exiba-o na tela
+        SwingUtilities.invokeLater(() -> {
+            new Breakout(gameController, 123); // Substitua 123 pelo valor real do seed
+        });
 	}
 
 }
